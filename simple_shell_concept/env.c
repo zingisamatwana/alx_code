1e67ed2e5cf3017014877cca37ceb93b75577f76
Zingisa Matwana <zingisamatwana@gmail.com>
2023-11-05 14:38:11 +0000

    Z: Updated files

--- simple_shell_concept/env.c
+++ simple_shell_concept/env.c
@@ -1,4 +1,4 @@
-#include <stdlib.h>
+y#include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
 #include <stdio.h>
@@ -13,10 +13,10 @@
  */
 int main(int argc, char *argv[], char *envp[])
 {
-	int lines, i, len_t, fd;
+/*	int line_t, i, len_t, fd;
 	char *path;
 
-	lines = 1;
+	line_t = 1;
 	fd = open("variable.txt", O_WRONLY | O_CREAT, 0777);
 	path = envp[21];
 	len_t = strlen(envp[21]);
@@ -25,18 +25,22 @@ int main(int argc, char *argv[], char *envp[])
 		if (path[i] == ':')
 		{
 			i++;
-			lines++;
+			line_t++;
 			write(fd, "\n", 1);
 		}
 		write(fd, &path[i], 1);
 	}
 	close(fd);
-
+*/
 	FILE *stream;
 	char *line = NULL;
 	size_t len = 0;
 	ssize_t nread;
+	char *patharr[50];
+	int c;
 
+	c = 0;
+	
 	stream = fopen("variable.txt", "r");
 	if (!stream)
 	{
@@ -45,17 +49,68 @@ int main(int argc, char *argv[], char *envp[])
 	}
 	while ((nread = getline(&line, &len, stream)) != EOF)
 	{
-		printf("Retrieved line of length %zu:\n", nread);
-		char lines[nread + 3];
-		strcpy(lines, line);
+//		printf("Retrieved line of length %zu:\n", nread);
+		//	char lines[nread + 3];
+		
+		//strcpy(lines, line);
+		/*
 		lines[nread - 1] = '/';
 		lines[nread ] = 'l';
 		lines[nread + 1] = 's';
 		lines[nread + 2] = '\n';
 		lines[nread + 3] = '\0';
-		fwrite(lines, nread + 3, 1, stdout);
+		*/
+		patharr[c] = strdup(line);
+			
+			//patharr[c] = line;
+		c++;
+		//fwrite(lines, nread + 3, 1, stdout);
 	}
 	fclose(stream);
-
+	for (int i = 0; i < c; i++)
+	{
+		printf("%d: %s", i, patharr[i]);
+	}
+	printf("\n ----------------------------------------------------------------\n");
+	
+	for (int i = 0; i < c; i++)
+	{
+		char *usr = "ls";
+		int pln = strlen(patharr[i]) - 2;
+		int usrln = strlen(usr);
+		char s[pln + usrln];
+		char *arr = patharr[i];
+		if (arr[pln-1]=='/')
+		{
+			int j;
+			
+		        for (j = 0; j < pln; j++)
+			{
+				s[j] = arr[j];
+			}
+			for (; j < pln + usrln; j++)
+			{
+				s[j] = usr[j - pln - 1];
+			}
+			s[j] = '\0';
+		}
+		else
+		{
+			int j;
+			for (j = 0; j < pln; j++)
+			{
+				s[j] = arr[j];
+			}
+			s[++j] = '/';
+			for (; j < pln + 1 + usrln; j++)
+			{
+				s[j] = usr[j - pln];
+			}
+			s[pln + usrln - 1] = '\0';
+		}
+		printf("%s\n", s);
+	}
+	
 	exit(EXIT_SUCCESS);
 }
+		
